972b0678c81d9f90c4a7f23d9b03d73c8244f16c
martenjuus <122257508+martenjuus@users.noreply.github.com>
2024-02-23 10:26:31 +0100

    removed srpties testing .😅😅😅😅😅😅

--- PlaceHolderSprite.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-var player : Node
-
-func _process(delta):
-	if player:
-		global_position = player.global_position
-		look_at(player.global_position)
--- Sprite3D.gd
+++
@@ -1,8 +0,0 @@
-extends Sprite3D
-
-var player : Node
-
-func _process(delta):
-	# Make the sprite face the same direction as the player
-	if player:
-		rotation = player.rotation
