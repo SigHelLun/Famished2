ebf5c52a7ea54ef17a7fe01cb025c4806c9ae534
SigHelLun <116567255+SigHelLun@users.noreply.github.com>
2024-04-09 10:43:18 +0200

    update interact message unique in progress

--- Items/Interactable.gd
+++ Items/Interactable.gd
@@ -1,2 +1,11 @@
 class_name Interactable
 extends StaticBody3D
+
+@export var promptMessege = 'Interact'
+@export var promptAction = 'interact'
+
+#func get_prompt():
+#	var key_name = ""
+##		if action is InputEventKey:
+#			key_name = OS.get_scancode_string(action.scancode)
+#	return promptMessege + "\n["+ key_name +"]"
