e2de8a58c5d8a65e54df18df054aa4d9adc971ec
Sigurd <116567255+SigHelLun@users.noreply.github.com>
2024-04-16 19:36:21 +0200

    animation update working walking idle

--- player.gd
+++ player.gd
@@ -7,12 +7,8 @@ const JUMP_VELOCITY = 3
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var neck := $Neck
 @onready var camera := $Neck/Camera3D
-#@onready var interaction_area: interactionArea = $interactionArea3d
-#func _ready():
-#	interaction_area.interact = Callable(self, "_on_interact")
+@onready var anim = $Neck/animationplayerfamished2/AnimationPlayer
 
-#func _on_interact():
-#	get_tree().change_scene_to_file("res://basement.tscn")
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -24,21 +20,20 @@ func _unhandled_input(event: InputEvent) -> void:
 			camera.rotate_x(-event.relative.y * 0.01)
 			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(60))
 
-
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
+	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		anim.play("walkingInplace")
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
+		anim.play("idle_001")  # Play the idle animation
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
 	move_and_slide()
