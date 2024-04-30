50095efb55149881a48b78f7e735cb5922aeb306
martenjuus <122257508+martenjuus@users.noreply.github.com>
2024-04-30 09:25:15 +0200

    fixed menu :de:

--- paused.gd
+++ paused.gd
@@ -1,24 +1,27 @@
 extends Control
 
-var is_paused = false :
-	set(value):
-		is_paused = value
-		get_tree().paused = is_paused
-		visible = is_paused
+var is_paused = false
 
-func _unhandled_input(event):
-	if event.is_action_pressed("pause"):
-		self.is_paused = !is_paused
-
+# Setter for is_paused variable
 func set_is_paused(value):
 	is_paused = value
 	get_tree().paused = is_paused
 	visible = is_paused
-	
+	if is_paused:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
+# Handle input events
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		is_paused = !is_paused
+		set_is_paused(is_paused)
+
+# Handle "Back to Game" button press
 func _on_back_2_game_pressed():
-	self.is_paused = false
+	set_is_paused(false)
 
-
+# Handle "Quit" button press
 func _on_quit_pressed():
 	get_tree().quit()
