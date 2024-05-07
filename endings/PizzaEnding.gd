ae173615ac1ebc06745cf4b1b4ba5e1a278a50cb
Oliver <oliverplayz34@gmail.com>
2024-05-07 13:04:00 +0200

    pizza ending and shelf collision :bus:

---
+++ endings/PizzaEnding.gd
@@ -0,0 +1,8 @@
+extends Control
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://select.tscn")
+	save.hideMe()
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
