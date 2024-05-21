f710453285b9badd0939b870cffd56a1995018f8
Sigurd <116567255+SigHelLun@users.noreply.github.com>
2024-05-21 11:08:43 +0200

    should work ignore errors

--- endings/CookieEnd.gd
+++ endings/CookieEnd.gd
@@ -3,6 +3,6 @@ extends Control
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://select.tscn")
-	save.hideMe()
+	save.hideC()
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
